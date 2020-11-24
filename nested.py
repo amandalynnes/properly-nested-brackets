bdeab8b328724a2c1d9527180b1ab75c5145c1a3
Amanda Simmons <carpe737diem@gmail.com>
2020-11-24 14:30:13 -0500

    added a stack to isnested function and printing it

--- nested.py
+++ nested.py
@@ -4,20 +4,32 @@
 Module docstring: My program reads a file, tests it, and writes results.
 """
 
-__author__ = "Amanda Simmons"
+__author__ = "Amanda Simmons, JT"
 
 import sys
+
+# read input.txt file
+# print to terminal and write results to output.txt file
+# check if all brackets in input.txt file are nested properly
+# possibly store example brackets in a dictionary/ list?
+# use a list as a stack
+# look up the shunting yard algorithm
 
 
 def is_nested(line):
     """Validate a single input line for correct nesting"""
-    pass
+    stack = []
+    for char in line:
+        stack.append(char)
+    print(stack)
 
 
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
     # Results: print to console and also write to output file
-    pass
+    with open('input.txt', 'r') as f:
+        for line in f:
+            is_nested(line.strip())
 
 
 if __name__ == '__main__':
